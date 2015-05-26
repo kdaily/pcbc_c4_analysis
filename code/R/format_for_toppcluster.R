0b4affc6a2e0248524d11e16388b76e4ce06939a
kdaily <kdaily@users.noreply.github.com>
2015-05-26 13:20:03 -0700

    updated doc

--- code/R/format_for_toppcluster.R
+++ code/R/format_for_toppcluster.R
@@ -4,14 +4,17 @@ library(tidyr)
 library(readr)
 library(stringr)
 library(reshape2)
+library(rGithubClient)
 
 library(synapseClient)
 synapseLogin()
 
+# Read in results of differential expression
 obj <- synGet("syn4216912")
 
 d <- read_tsv(getFileLocation(obj))
 
+# Separate out the gene names for each contrast
 tolong <- function(x) {
   contr <- unique(x$Contrast.Names)
   genes <- str_split(x$Gene.Symbols, ",")
@@ -22,16 +25,23 @@ tolong <- function(x) {
 
 d2 <- ddply(d, .(Contrast.Names), tolong, .progress='text')
 
-d2.up <- d2 %>% 
+# Split between up and down-regulated
+d2up <- d2 %>% 
   filter(str_detect(Contrast.Names, "up"), !is.na(gene), gene != "") %>%
   select(gene, Contrast.Names)
 
-d2.down <- d2 %>% 
+d2down <- d2 %>% 
   filter(str_detect(Contrast.Names, "down"), !is.na(gene), gene != "") %>%
   select(gene, Contrast.Names)
 
-write.table(d2.up, file="DiffnameShort_DiffGenes_Express_mixedEffects_up_toppcluster.csv",
+write.table(d2up, file="DiffnameShort_DiffGenes_Express_mixedEffects_up_toppcluster.csv",
             sep=",", row.names=FALSE, col.names=FALSE, quote=FALSE)
 
-write.table(d2.down, file="DiffnameShort_DiffGenes_Express_mixedEffects_down_toppcluster.csv",
+fUp <- File("DiffnameShort_DiffGenes_Express_mixedEffects_up_toppcluster.csv", parentId='syn3471223')
+fUp <- synStore(fUp, used=obj@properties$id, executed=thisScript)
+
+write.table(d2down, file="DiffnameShort_DiffGenes_Express_mixedEffects_down_toppcluster.csv",
             sep=",", row.names=FALSE, col.names=FALSE, quote=FALSE)
+
+fDown <- File("DiffnameShort_DiffGenes_Express_mixedEffects_up_toppcluster.csv", parentId='syn3471223')
+fDown <- synStore(fDown, used=obj@properties$id, executed=thisScript)
