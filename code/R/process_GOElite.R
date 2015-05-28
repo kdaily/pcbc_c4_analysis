000fcabea6060490da867ec667efb200210eec47
kdaily <kdaily@users.noreply.github.com>
2015-05-28 10:04:33 -0700

    updated to use synapse

--- code/R/process_GOElite.R
+++ code/R/process_GOElite.R
@@ -3,10 +3,17 @@ library(dplyr)
 library(tidyr)
 library(data.table)
 library(reshape2)
+library(synapseClient)
+library(rGithubClient)
 
-infile <- '/home/kdaily/Projects/PCBC/pcbc_c4_analysis/code/pipelines/GOElite/output/final/results.tsv'
+thisRepo <- getRepo("kdaily/pcbc_c4_analysis", ref="branch", refName="goelite")
+thisScript <- getPermlink(repo=thisRepo, path="code/R/process_GOElite.R")
 
-d <- fread(infile, data.table=FALSE)
+synapseLogin()
+
+inObj <- synGet('syn4228803')
+
+d <- fread(getFileLocation(inObj), data.table=FALSE)
 
 # Clean up column names
 colnames(d) <- gsub("[- ]", "_", colnames(d))
@@ -14,29 +21,28 @@ colnames(d) <- gsub("[- ]", "_", colnames(d))
 # Filter, select columns
 d2 <- d %>%
   filter(Z_Score > 0, Number_Changed > 4, PermuteP < 0.05) %>%
-  select(Gene_Set_Name, contrast, geneset, symbol, AdjustedP) %>%
-  tidyr::unite(group, geneset, Gene_Set_Name, contrast, sep="_", remove = FALSE)
+  select(Gene_Set_Name, contrast, geneset, Z_Score, AdjustedP) %>%
+  tidyr::unite(GS, Gene_Set_Name, geneset, sep="_")
 
-# cast into a wide matrix
 d3 <- d2 %>% 
-  select(contrast, geneset, Gene_Set_Name, AdjustedP) %>% 
-  unique() %>% 
-  dcast(., geneset + Gene_Set_Name ~ contrast, value.var = "AdjustedP")
+  dcast(GS ~ contrast, value.var="Z_Score")%>%
+  select(-contains("ale"), -contains("plasmid"), -contains("virus"), 
+         -contains("plasmid"), -contains("NotApplicable"))
+
+colnames(d3)
 
 # Any NA's get a p-value of 1
 d3[is.na(d3)] <- 1
 
 # Convert to log_10 for clustering
-d3[, -(1:2)] <- -log10(d3[, -(1:2)])
+forclust <- -log10(d3[, -1])
+rownames(forclust) <- d3$GS
 
-cluster.cols <- hclust(dist(t(d3[, -(1:2)])))
+cluster.cols <- hclust(dist(forclust), method = "ward.D2")
+
+pdf("DiffState_GOElite_cluster.pdf", width=100, height=20)
 plot(cluster.cols)
+dev.off()
 
-tofile <- function(x, dir) {
-  group <- unique(x$group)
-  group <- gsub("[]")
-  fn <- paste0(group, ".tsv")
-  write.table(x, file=fn, sep="\t", row.names=F, quote=F)
-}
-
-d_ply(d2, .(group), tofile, dir="/home/kdaily/Projects/PCBC/pcbc_c4_analysis/code/pipelines/GOElite/output/toppcluster/")
+f <- File("DiffState_GOElite_cluster.pdf", parentId='syn4228846')
+f <- synStore(f, used=inObj@properties$id, executed=thisScript)
