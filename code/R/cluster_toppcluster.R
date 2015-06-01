448d9b86e886536b9a32cd1bdb6a98f3741e7b34
kdaily <kdaily@users.noreply.github.com>
2015-06-01 13:10:54 -0700

    reorg, remove unused code, do clustering

--- code/R/cluster_toppcluster.R
+++ code/R/cluster_toppcluster.R
@@ -1,7 +1,7 @@
 library(plyr)
 library(dplyr)
 library(tidyr)
-library(readr)
+library(data.table)
 library(stringr)
 library(reshape2)
 
@@ -9,28 +9,67 @@ library(synapseClient)
 synapseLogin()
 
 # Requires file output from ToppCluster
-d.up <- read_tsv("/home/kdaily/Projects/PCBC/pcbc_c4_analysis/code/pipelines/GOElite/output/toppcluster/results/up.tsv")
-d.down <- read_tsv("/home/kdaily/Projects/PCBC/pcbc_c4_analysis/code/pipelines/GOElite/output/toppcluster/results/down.tsv")
+# These give warnings on many rows
+dUpObj <- synGet("syn4231394")
+dDownObj <- synGet("syn4231385")
 
-idxcols <- c("Category", "ID", "Title (or Source)")
-d.idx <- d.up %>%
-  select(one_of(idxcols))
+# tmp <- read.delim(getFileLocation(dUpObj), sep="\t", nrows=200)
+# Use to get column classes
+# sapply(tmp, class)
 
-d2 <- d.up %>% 
-  full_join(d.down, by=idxcols) %>% 
-  dplyr::select(1:3, ends_with("logP"))
+colTypes <- c(rep("character", 4),
+              rep("numeric", sum(str_detect(colnames(tmp), "logP"))),
+              rep("character", sum(str_detect(colnames(tmp), "_GeneSet$"))))
 
-d.dist <- dist(t(d2[, -(1:3)])) # euclidean distances between the rows
-fit <- cmdscale(d.dist, eig=TRUE, k=2) # k is the number of dim
-fit # view results
+d.up <- fread(getFileLocation(dUpObj), colClasses=colTypes, data.table=F)
+d.down <- fread(getFileLocation(dDownObj), colClasses=colTypes, data.table=F)
 
-# plot solution
+# Clean up columns
+colnames(d.up)[1:4] <- c("Category", "ID", "Title", "VerboseID")
+colnames(d.down)[1:4] <- c("Category", "ID", "Title", "VerboseID")
 
-x <- fit$points[, 1]
-y <- fit$points[, 2]
-labels <- rownames(fit$points)
+idxcols <- c("Category", "ID", "Title", "VerboseID")
 
-plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2", 
-     main="Metric MDS", type="n")
+d2 <- d.up %>%
+  full_join(d.down, by=idxcols) %>% # join up and down together
+  dplyr::select(VerboseID, ends_with("logP")) %>% # only get id and value columns
+  filter(!is.na(VerboseID)) # some NAs to get rid of
 
-text(x, y, labels = labels, cex=.7)
+# Clean up column names again to make them easier to read
+colnames(d2) <- str_replace(colnames(d2), "_logP", "")
+
+# Use only the diff state columns
+d3 <- d2 %>% select(-contains("ale"), -contains("virus"), 
+                    -contains("plasmid"), -contains("NotApplicable")) 
+
+# Matrix for clustering
+forclust <- as.data.frame(d3[, -1])
+rownames(forclust) <- d3$VerboseID
+forclust[is.na(forclust)] <- 0
+
+# Only keep categories with a minimum number of significant differences
+minDifferences <- 2
+keep <- apply(forclust, 1, function(x) sum(x > 0) > minDifferences)
+forclust2 <- forclust[keep, ]
+
+# standard hierarchical clustering with euclidean distances
+d.dist <- dist(forclust2)
+cluster.cols <- hclust(d.dist)
+
+# Not even useful
+pdf("DiffState_ToppCluster_cluster.pdf", width=100, height=20)
+plot(cluster.cols)
+dev.off()
+
+plot.data <- data.frame(forclust2, VerboseID=rownames(forclustBinary))
+plot.data <- melt(plot.data, id.vars="VerboseID")
+plot.data$direction <- "up"
+plot.data$direction[str_detect(plot.data$variable, "down")] <- "down"
+
+plot.data$VerboseID <- factor(plot.data$VerboseID, levels=cluster.cols$labels, ordered=TRUE)
+plot.data$VerboseID2 <- as.numeric(plot.data$VerboseID)
+p <- ggplot(plot.data, aes(x=VerboseID, y=variable, fill=value)) + geom_tile()
+p <- p + facet_grid(direction ~ ., scale="free_y")
+p <- p + scale_fill_gradient(low='white', high='black')
+p <- p + theme(axis.text.x=element_blank())
+p
