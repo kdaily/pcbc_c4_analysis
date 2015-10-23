0058c708f13cf8d11bfe3315d2408e4e3211ce83
kdaily <kdaily@users.noreply.github.com>
2015-10-23 11:16:22 -0700

    only do effCounts for merging transcripts to genes

--- code/R/eXpress_transcripts_to_gene.R
+++ code/R/eXpress_transcripts_to_gene.R
@@ -23,23 +23,13 @@ mappingData <- tbl_df(fread(getFileLocation(mappingFile), data.table=FALSE))
 mappingData <- mappingData %>% rename(target_id=hg19.knownGene.name)
 
 ## Set up provenance
-repo <- getRepo("kdaily/pcbc_c4_analysis", ref="branch", refName="eXpress_synapse")
+repo <- getRepo("Sage-Bionetworks/pcbc_c4_analysis")
 thisFile <- getPermlink(repo, "code/R/eXpress_transcripts_to_gene.R")
 
 ## Set up annotations
 annots <- list(dataType="mRNA", fileType="genomicMatrix", expressionLevel="gene")
 
-tpmID <- "syn3354728"
-tpmData <- collapse_eXpress(tpmID, mappingData)
-write.csv(tpmData, "eXpress_tpm_geneSymbol.csv", row.names=FALSE)
-f <- File("eXpress_tpm_geneSymbol.csv", parentId="syn3354743")
-synSetAnnotations(f) <- annots
-generatedBy(f) <- Activity(used=c(tpmID, mappingFile), 
-                           executed=thisFile,
-                           name="Sum", description="Sum over gene symbol")
-f <- synStore(f)
-
-effCountID <- "syn3354716"
+effCountID <- "syn5006129"
 effCountData <- collapse_eXpress(effCountID, mappingData)
 write.csv(effCountData, "eXpress_eff_count_geneSymbol.csv", row.names=FALSE)
 f <- File("eXpress_eff_count_geneSymbol.csv", parentId="syn3354743")
@@ -47,25 +37,4 @@ synSetAnnotations(f) <- annots
 generatedBy(f) <- Activity(used=c(effCountID, mappingFile), 
                                     executed=thisFile,
                                     name="Sum", description="Sum over gene symbol")
-f <- synStore(f)
-
-
-estCountID <- "syn3354714"
-estCountData <- collapse_eXpress(estCountID, mappingData)
-write.csv(estCountData, "eXpress_est_count_geneSymbol.csv", row.names=FALSE)
-f <- File("eXpress_est_count_geneSymbol.csv", parentId="syn3354743")
-synSetAnnotations(f) <- annots
-generatedBy(f) <- Activity(used=c(estCountID, mappingFile), 
-                           executed=thisFile,
-                           name="Sum", description="Sum over gene symbol")
-f <- synStore(f)
-
-fpkmID <- "syn3354736"
-fpkmData <- collapse_eXpress(fpkmID, mappingData)
-write.csv(fpkmData, "eXpress_fpkm_geneSymbol.csv", row.names=FALSE)
-f <- File("eXpress_fpkm_geneSymbol.csv", parentId="syn3354743")
-synSetAnnotations(f) <- annots
-generatedBy(f) <- Activity(used=c(fpkmID, mappingFile), 
-                           executed=thisFile,
-                           name="Sum", description="Sum over gene symbol")
 f <- synStore(f)
