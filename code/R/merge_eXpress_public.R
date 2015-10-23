da2e7019973ae901e420157fdbb42df57b942230
kdaily <kdaily@users.noreply.github.com>
2015-10-23 10:33:31 -0700

    fix adding annotations

--- code/R/merge_eXpress_public.R
+++ code/R/merge_eXpress_public.R
@@ -48,24 +48,28 @@ mergedFile <- synStore(mergedFile, used="syn3270268", executed=thisScript)
 
 estCounts <- dcast(pp1, target_id ~ UID, value.var="est_counts")
 write.csv(estCounts, "eXpress_est_counts.csv", row.names=FALSE)
-estCountsFile <- File("eXpress_est_counts.csv", parentId="syn3354743",
-                      annotations=list(fileType="genomicMatrix", dataType="mRNA", expressionLevel="transcript"))
+estCountsFile <- File("eXpress_est_counts.csv", parentId="syn3354743")
+synSetAnnotations(estCountsFile) <- list(fileType="genomicMatrix", dataType="mRNA", 
+                                         expressionLevel="transcript")
 estCountsFile <- synStore(estCountsFile, used=mergedFile@properties$id, executed=thisScript)
 
 effCounts <- dcast(pp1, target_id ~ UID, value.var="eff_counts")
 write.csv(effCounts, "eXpress_eff_counts.csv", row.names=FALSE)
-effCountsFile <- File("eXpress_eff_counts.csv", parentId="syn3354743",
-                      annotations=list(fileType="genomicMatrix", dataType="mRNA", expressionLevel="transcript"))
+effCountsFile <- File("eXpress_eff_counts.csv", parentId="syn3354743")
+synSetAnnotations(effCountsFile) <- list(fileType="genomicMatrix", dataType="mRNA", 
+                                         expressionLevel="transcript")
 effCountsFile <- synStore(effCountsFile, used=mergedFile@properties$id, executed=thisScript)
 
 tpm <- dcast(pp1, target_id ~ UID, value.var="tpm")
 write.csv(tpm, "eXpress_tpm.csv", row.names=FALSE)
-tpmFile <- File("eXpress_tpm.csv", parentId="syn3354743",
-                annotations=list(fileType="genomicMatrix", dataType="mRNA", expressionLevel="transcript"))
+tpmFile <- File("eXpress_tpm.csv", parentId="syn3354743")
+synSetAnnotations(tpmFile) <- list(fileType="genomicMatrix", dataType="mRNA", 
+                                   expressionLevel="transcript")
 tpmFile <- synStore(tpmFile, used=mergedFile@properties$id, executed=thisScript)
 
 fpkm <- dcast(pp1, target_id ~ UID, value.var="fpkm")
 write.csv(fpkm, "eXpress_fpkm.csv", row.names=FALSE)
-fpkmFile <- File("eXpress_fpkm.csv", parentId="syn3354743",
-                 annotations=list(fileType="genomicMatrix", dataType="mRNA", expressionLevel="transcript"))
+fpkmFile <- File("eXpress_fpkm.csv", parentId="syn3354743")
+synSetAnnotations(fpkmFile) <- list(fileType="genomicMatrix", dataType="mRNA", 
+                                    expressionLevel="transcript")
 fpkmFile <- synStore(fpkmFile, used=mergedFile@properties$id, executed=thisScript)
